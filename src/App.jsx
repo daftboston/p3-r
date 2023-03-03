85d451405a66d0e64675b94aaa8a285ac71b768e
Daniel <dsantoyop@gmail.com>
2023-03-02 20:09:43 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
 import { Component, useState } from 'react'
 
 import './App.css'
-import phrases from "./data/phrases.json"
-import CookieCard from './components/CookieCard'
-import fondo1 from "./assets/fondo1.png"
-import fondo2 from "./assets/fondo2.png"
-import fondo3 from "./assets/fondo3.png"
-import fondo4 from "./assets/fondo4.png"
+
+
 
 
 function App() {
 
-  const backgrounds = [fondo1,fondo2, fondo3, fondo4,fondo1,fondo2, fondo3, fondo4,fondo1,fondo2, fondo3, fondo4,fondo1,fondo2, fondo3]
-
+ 
 
   const [index, setindex]=useState(0)
-  const changeIndex=()=>{
-    setindex(Math.floor(Math.random()*phrases.length))
-  }
 
 
 
   return (
-    <div className="App" style={{
-        backgroundImage:`url(${backgrounds[index]})`,      
-     }}>
-         <CookieCard dataCookie={phrases[index]}/>
-         <button className='button' onClick={changeIndex}> Probar mi suerte</button>
+    <div className="App">
+        
     
     </div>
   )
